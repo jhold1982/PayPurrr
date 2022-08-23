be36f71b4096ed0393d2d8e79e6e95a1ce19b639
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-08-23 13:05:00 -0500

    Initial Commit

---
+++ PayPurrr/PayPurrrApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayPurrrApp.swift
+//  PayPurrr
+//
+//  Created by Justin Hold on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct PayPurrrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
